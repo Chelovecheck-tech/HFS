efd581e9e86f6ac53a82bb2dc258a15283e42f26
Chelovecheck-tech <ali.azamatov11@gmail.com>
2026-01-11 17:22:11 +0600

    new urls

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://api.nbb.kg';
+const API_URL = import.meta.env.VITE_API_URL || 'https://hfs-backend-production.up.railway.app';
 
 export default API_URL;
